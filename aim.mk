7dc8b198dc5819559cfdc9da5a581bfd3ef98db8
mocarafee <harif.rp05@gmail.com>
2020-05-28 03:30:00 +0700

    klimlte: aim bringup

--- aim.mk
+++ aim.mk
@@ -7,13 +7,13 @@ $(call inherit-product, device/samsung/klimtlte/device.mk)
 
 PRODUCT_BRAND := samsung
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common AIM stuff.
+$(call inherit-product, vendor/aim/config/common_full_tablet_wifionly.mk)
 
-# Inherit more LineageOS stuff.
-$(call inherit-product, vendor/cm/config/telephony.mk)
+# Inherit more AIM stuff.
+$(call inherit-product, vendor/aim/config/telephony.mk)
 
-PRODUCT_NAME := lineage_klimtlte
+PRODUCT_NAME := aim_klimtlte
 PRODUCT_DEVICE := klimtlte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
